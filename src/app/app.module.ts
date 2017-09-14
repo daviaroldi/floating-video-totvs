a45686c4cdb4769325c96f55daf2e4c35ab990e0
Davi Aroldi <davi.aroldi@gmail.com>
2017-09-13 23:41:09 -0300

    alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { AngularDraggableModule } from 'angular2-draggable';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './../pages/main.component';
+
+const appRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'app',  component: AppComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    AngularDraggableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/pages/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+  title = 'funfou2';
+}
